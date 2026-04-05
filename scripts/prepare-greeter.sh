84e708aaada2d9f66b27df3e78ae2b88e417a359
zach <zach@brohn.se>
2026-04-05 15:11:45 +0200

    fix prepare-greeter.sh

--- scripts/prepare-greeter.sh
+++ scripts/prepare-greeter.sh
@@ -4,14 +4,12 @@ BINARY="../Greeter/scripts/start-zshell-greeter"
 CONFIG="../Greeter/scripts/zshell-hyprland.conf"
 GREETD_CONFIG="../Greeter/scripts/greeter-config.toml"
 WALLPAPER="$HOME/.local/state/zshell/lockscreen_bg.png"
-FACE="$HOME/.face"
 
 main() {
 	sudo mkdir -p "/etc/zshell-greeter/images"
 	sudo cp "$BINARY" "/usr/bin"
 	sudo cp "$CONFIG" "/etc/zshell-greeter"
 	sudo cp "$WALLPAPER" "/etc/zshell-greeter/images/greeter_bg.png"
-	sudo cp "$FACE" "/etc/zshell-greeter/images/face"
 	sudo cp "$GREETD_CONFIG" "/etc/greetd/config.toml"
 }
 
