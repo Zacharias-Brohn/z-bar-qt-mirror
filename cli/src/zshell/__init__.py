17fcf1a02c7f066ce91b97e0f125d02a57294fc2
AramJonghu <aramjonghu@tutamail.com>
2026-05-25 18:42:34 +0200

    pyright error fixes. added autocomplete to some commands

--- cli/src/zshell/__init__.py
+++ cli/src/zshell/__init__.py
@@ -2,14 +2,13 @@ from __future__ import annotations
 import typer
 from zshell.subcommands import shell, scheme, screenshot, wallpaper, record
 
-app = typer.Typer()
+app = typer.Typer(name="zshell-cli")
 
 app.add_typer(shell.app, name="shell")
 app.add_typer(scheme.app, name="scheme")
 app.add_typer(screenshot.app, name="screenshot")
 app.add_typer(wallpaper.app, name="wallpaper")
 app.add_typer(record.app, name="record")
-# app.add_typer(preset.app, name="preset")
 
 
 def main() -> None:
